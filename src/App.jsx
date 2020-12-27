a9f5c15079171c74d3e5ec208f40ca97c57c3aa3
渡辺大地 <watanabedaichi@watanabedaichinoMacBook-Pro.local>
2020-12-27 12:17:35 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
 import React from 'react';
+import axios from 'axios';
 import './index.css';
 // components
-import {SearchForm} from './compornents/SearchForm';
-import {ResultList} from './compornents/ResultList';
+// import {SearchForm} from './compornents/SearchForm';
+// import {ResultList} from './compornents/ResultList';
+
+const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+export default class App extends React.Component {
+  state = {
+    videos: [],
+    keyword: 'Create-React-APP'
+  }
+
+  componentDidMount() {
+    const url = `https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q=${this.state.keyword}&maxResults=3&key=${YOUTUBE_API_KEY}`;
+
+    axios
+      .get(url)
+      .then(response => {
+          this.setState({
+            videos: response.data.items,
+          });
+      })
+      .catch(() => {
+          console.log('通信に失敗しました');
+      });
+  }
+
+  render() {
+    console.log(this.state.videos)
+    return (
+      <>
+       <h1>テキストが入ります</h1>
+      </>
+    )
+  }
+}
 
 
-export const App = () => {
+// export const App = () => {
 
-  return(
-    <div className="app">
-      <SearchForm />
-      <ResultList />
-    </div>
-  );
-};
+//   return(
+//     <div className="app">
+//       <SearchForm />
+//       <ResultList />
+//     </div>
+//   );
+// };
 // 次回コンポーネントの読み込みからスタート
--- src/compornents/SearchForm.jsx
+++ src/compornents/SearchForm.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
 
-export const SearchForm = () =>{
-  const SerachWordValue = "";
+export class SearchForm extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {value: ""}
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  
+  render() {
+    return(
+      <div className="SearchForm">
+        <div className="SerachWordWapper flex">
+          <input 
+            type="text" 
+            value={this.state.value} 
+            onChange={this.handleChange}
+            placeholder="テキストを入力"
+          />
+          <p className="cancel">キャンセル</p>
+        </div>
+        <div className="SerchServiceList flex">
+          <p className="SerchService active">All</p>
+          <p className="SerchService">はてなブログ</p>
+          <p className="SerchService">YOUTUBE</p>
+          <p className="SerchService">Google</p>
+          <p className="SerchService">Google</p>
+          <p className="SerchService">Google</p>
+        </div>
+      </div>
+    );
+  }
 
   // handleChange(){
   //   const handleChange = (event) => SetSearchWordText(event.target.value);
   // }
 
-  return(
-    <div className="SearchForm">
-      <div className="SerachWordWapper flex">
-        <input 
-          type="text" 
-          value={SerachWordValue} 
-          // onChange={handleChange}
-          placeholder="テキストを入力"
-        />
-        <p className="cancel">キャンセル</p>
-      </div>
-      <div className="SerchServiceList flex">
-        <p className="SerchService active">All</p>
-        <p className="SerchService">はてなブログ</p>
-        <p className="SerchService">YOUTUBE</p>
-        <p className="SerchService">Google</p>
-        <p className="SerchService">Google</p>
-        <p className="SerchService">Google</p>
-      </div>
-    </div>
-  );
 }
